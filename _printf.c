8d06d8f2dd23d9998ce2ed04f7a100233fd81ff7
root <ilyesmazouzz@gmail.com>
2023-03-29 14:47:15 -0700

    customized printf

--- _printf.c
+++ _printf.c
@@ -15,6 +15,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+	if (!format || (*format == '%' && !*(format + 1)))
+		return (-1);
 	while (*format)
 	{
 	if (*format == '%')
