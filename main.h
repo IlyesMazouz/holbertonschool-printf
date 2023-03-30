9543523b7e12d29d90ec69855056cd934aed40f5
root <ilyesmazouzz@gmail.com>
2023-03-30 04:04:55 -0700

    header fil

--- main.h
+++ main.h
@@ -1,7 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int main(void);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+typedef struct format
+{
+	char ch;
+	int (*f)(va_list);
+} format_specifier;
+
+int get_format_specifier(const char c, va_list args);
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
 
 #endif
